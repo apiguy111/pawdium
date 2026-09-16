feb1054a8cb967285817b1a7385a9cbcc03591bd
Kuwar shiv pratap singh <102828236+apiguy111@users.noreply.github.com>
2026-09-17 00:02:24 +0530

    Refactor Firebase initialization code

--- Server/config/firebase.js
+++ Server/config/firebase.js
@@ -1,10 +1,8 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getStorage } = require("firebase-admin/storage");
 
-const serviceAccount = require("../firebase-service-account.json");
 
 const app = initializeApp({
-    credential: cert(serviceAccount),
     storageBucket: "pawdium-e4710.firebasestorage.app",
 });
 
